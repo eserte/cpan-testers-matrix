42b20437d00834ac881424fa3c1ea5486881504a
Slaven Rezic <slaven@rezic.de>
2010-01-22 21:40:56 +0000

    * analysis site is now beta

--- cpantestersmatrix.pl
+++ cpantestersmatrix.pl
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: cpantestersmatrix.pl,v 1.117 2010/01/03 10:15:55 eserte Exp $
+# $Id: cpantestersmatrix.pl,v 1.118 2010/01/22 21:40:56 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2007,2008 Slaven Rezic. All rights reserved.
@@ -18,7 +18,7 @@ package # not official yet
 
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%03d", q$Revision: 1.117 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%03d", q$Revision: 1.118 $ =~ /(\d+)\.(\d+)/);
 
 use vars qw($UA);
 
@@ -1202,7 +1202,7 @@ sub dist_links {
 EOF
     if (defined $dist_version) {
 	print <<EOF;
-<li><a href="http://analysis.cpantesters.org/solved?distv=$dist-$dist_version">Reports analysis</a> (alpha alert!)
+<li><a href="http://analysis.cpantesters.org/solved?distv=$dist-$dist_version">Reports analysis</a> (beta)
 EOF
     }
     print <<EOF;
