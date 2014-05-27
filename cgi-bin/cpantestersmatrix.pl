a2beac555f65b964688c1efe82bd4afb5606325f
Slaven Rezic <slaven@rezic.de>
2014-05-27 23:32:02 +0200

    current stable perl is 5.20.0

--- cgi-bin/cpantestersmatrix.pl
+++ cgi-bin/cpantestersmatrix.pl
@@ -17,7 +17,7 @@ package # not official yet
 use strict;
 use warnings;
 use vars qw($VERSION);
-$VERSION = '2.09';
+$VERSION = '2.10';
 
 use vars qw($UA);
 
@@ -66,7 +66,7 @@ sub downtime_teaser ();
 my $cache_days = 1/8;
 my $ua_timeout = 10;
 
-my $current_stable_perl = "5.18.0"; # please always end with ".0"
+my $current_stable_perl = "5.20.0"; # please always end with ".0"
 
 #use constant FILEFMT_AUTHOR => 'yaml';
 use constant FILEFMT_AUTHOR => 'json';
