ccdef01b7c0f48ea133471de8f736b67011455bf
Slaven Rezic <slaven@rezic.de>
2024-07-14 13:38:07 +0200

    current stable perl is 5.40.0

--- cgi-bin/cpantestersmatrix.pl
+++ cgi-bin/cpantestersmatrix.pl
@@ -18,7 +18,7 @@ use 5.010; # defined-or
 use strict;
 use warnings;
 use vars qw($VERSION);
-$VERSION = '2.55';
+$VERSION = '2.56';
 
 use vars qw($UA);
 
@@ -69,7 +69,7 @@ sub _reusing_cache_msg ($);
 my $cache_days = 1/8;
 my $ua_timeout = 30;
 
-my $current_stable_perl = "5.38.0"; # please always end with ".0"
+my $current_stable_perl = "5.40.0"; # please always end with ".0"
 
 use constant USE_JQUERY_TABLESORTER => 1;
 
